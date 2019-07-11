fae7cb388c2ff6a7369bdfc11977808f772b4d94
truckee99 <47430935+truckee99@users.noreply.github.com>
2019-07-11 16:43:24 -0700

    Add project files.

---
+++ CSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0) { 
+            Console.WriteLine($"Hello World! {args[0]}");
+            }
+            else
+            {
+                Console.WriteLine($"Hello");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
